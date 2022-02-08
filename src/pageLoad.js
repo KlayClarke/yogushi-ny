03f6e7abbae6a7663323074a50b475ac76af1c06
Klay Clarke <klayaclarke@gmail.com>
2022-02-07 22:43:50 -0500

    complete landing page for yogushi restaurant (mock) - will work on individual tab pages next

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,14 +1,18 @@
 export function pageLoad() {
   const content = document.querySelector("#content");
+  const header = document.createElement("header");
+  const footer = document.createElement("footer");
   const top = document.createElement("div");
   const middle = document.createElement("div");
   const bottom = document.createElement("div");
-  const header = document.createElement("header");
-  const logo = document.createElement("h1");
   const btnRow = document.createElement("div");
+  const logo = document.createElement("h1");
   const yogaBtn = document.createElement("button");
   const sushiBtn = document.createElement("button");
   const locationsBtn = document.createElement("button");
+  const reservationLink = document.createElement("a");
+
+  reservationLink.innerText = "Reserve A Mat";
 
   top.classList.add("top");
   middle.classList.add("middle");
@@ -20,17 +24,21 @@ export function pageLoad() {
   yogaBtn.innerText = "Yoga";
   sushiBtn.innerText = "Sushi";
   locationsBtn.innerText = "Locations";
-
-  top.appendChild(header);
-
-  header.appendChild(logo);
-  header.appendChild(btnRow);
+  footer.innerHTML = "&#169 2022 Yogushi Group";
 
   btnRow.appendChild(yogaBtn);
   btnRow.appendChild(sushiBtn);
   btnRow.appendChild(locationsBtn);
 
+  header.appendChild(logo);
+  header.appendChild(btnRow);
+
+  bottom.appendChild(reservationLink);
+
+  top.appendChild(header);
+
   content.appendChild(top);
   content.appendChild(middle);
   content.appendChild(bottom);
+  content.appendChild(footer);
 }
